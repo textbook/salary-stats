b5f2ef66b0f7a1735ef80ab31714b2eca68c6e45
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2017-06-24 15:27:15 +0100

    Update coverage reporting for CI

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
       'text/x-typescript': ['ts','tsx']
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
