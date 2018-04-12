40752308d8536ea7f3676b485438f4bc86ea5c07
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-04-12 19:19:07 +0100

    Run Chrome tests with --no-sandbox to avoid travis-ci/travis-ci#8836

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,10 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: { base: 'ChromeHeadless', flags: ['--no-sandbox'] }
+    },
     singleRun: false
   });
 };
