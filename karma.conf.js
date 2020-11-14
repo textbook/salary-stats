4cc874197ba83aa6ff26a8c48e51d46bec8e218f
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-11-14 14:13:38 +0000

    Remove deprecated coverage reporter

--- karma.conf.js
+++ karma.conf.js
@@ -10,16 +10,16 @@ module.exports = function (config) {
       require('karma-mocha-reporter'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'),
-      fixWebpackSourcePaths: true,
-      reports: ['lcovonly', 'text-summary']
+    coverageReporter: {
+      dir : 'coverage/',
+      subdir: '.',
+      type : 'lcovonly'
     },
     angularCli: {
       environment: 'dev'
