87109f21293eb801fb194e4ad45751b9c525d596
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-01-04 12:32:09 +0000

    Trim unneeded parts of Jerzy

--- src/lib/jerzy/distributions.ts
+++ src/lib/jerzy/distributions.ts
@@ -1,6 +1,3 @@
-import { bisection } from "./numeric";
-import { Vector } from "./vector";
-
 /*
 * Kolmogorov distribution
 */
@@ -18,21 +15,7 @@ export class Kolmogorov {
 		return Math.sqrt(2 * Math.PI) * sum / x;
 	};
 	
-	distr(arg: Vector): Vector;
-	distr(arg: number): number;
-	distr(arg: Vector | number): Vector | number {
-		if (arg instanceof Vector) {
-			const result = new Vector([]);
-			for (let i = 0; i < arg.length(); ++i) {
-				result.push(this._di(arg.elements[i]));
-			}
-			return result;
-		} else {
-			return this._di(arg);
-		}
-	};
-	
-	inverse(x: number): number {
-		return bisection((y) => this._di(y) - x, 0, 1);
+	distr(arg: number): number {
+		return this._di(arg);
 	};
 }
--- src/lib/jerzy/vector.ts
+++ src/lib/jerzy/vector.ts
@@ -5,16 +5,8 @@ export class Vector {
     this.elements.push(value);
   }
 
-  map(func: (arg: number) => number): Vector {
-    return new Vector(this.elements.map(func));
-  }
-
   length(): number {
     return this.elements.length;
-  }
-
-  concat(x: Vector): Vector {
-    return new Vector(this.elements.slice(0).concat(x.elements.slice(0)));
   }
 
   abs(): Vector {
@@ -25,42 +17,11 @@ export class Vector {
     return new Vector(values);
   }
 
-  dot(v: Vector): number {
-    let result = 0;
-    for (let i = 0; i < this.length(); i++) {
-      result = result + this.elements[i] * v.elements[i];
-    }
-    return result;
-  }
-
-  sum(): number {
-    let sum = 0;
-    for (let i = 0, n = this.elements.length; i < n; ++i) {
-      sum += this.elements[i];
-    }
-    return sum;
-  };
-
-  log(): Vector {
+  add(term: Vector): Vector {
     const result = new Vector(this.elements.slice(0));
     const n = this.elements.length;
-    for (var i = 0; i < n; ++i) {
-      result.elements[i] = Math.log(result.elements[i]);
-    }
-    return result;
-  };
-
-  add(term: Vector | number): Vector {
-    const result = new Vector(this.elements.slice(0));
-    const n = this.elements.length;
-    if (term instanceof Vector) {
-      for (let i = 0; i < n; ++i) {
-        result.elements[i] += term.elements[i];
-      }
-    } else {
-      for (let i = 0; i < n; ++i) {
-        result.elements[i] += term;
-      }
+    for (let i = 0; i < n; ++i) {
+      result.elements[i] += term.elements[i];
     }
     return result;
   };
@@ -69,47 +30,13 @@ export class Vector {
     return this.add(term.multiply(-1));
   };
 
-  multiply(factor: Vector | number): Vector {
+  multiply(factor: number): Vector {
     const result = new Vector(this.elements.slice(0));
     const n = this.elements.length;
-    if (factor instanceof Vector) {
-      for (var i = 0; i < n; ++i) {
-        result.elements[i] = result.elements[i] * factor.elements[i];
-      }
-    } else {
-      for (var i = 0; i < n; ++i) {
-        result.elements[i] = result.elements[i] * factor;
-      }
+    for (var i = 0; i < n; ++i) {
+      result.elements[i] = result.elements[i] * factor;
     }
     return result;
-  };
-
-  pow(p: Vector | number): Vector {
-    const result = new Vector(this.elements.slice(0));
-    const n = this.elements.length;
-    if (p instanceof Vector) {
-      for (let i = 0; i < n; ++i) {
-        result.elements[i] = Math.pow(result.elements[i], p.elements[i]);
-      }
-    } else {
-      for (let i = 0; i < n; ++i) {
-        result.elements[i] = Math.pow(result.elements[i], p);
-      }
-    }
-    return result;
-  };
-
-  mean(): number {
-    let sum = 0;
-    const n = this.elements.length;
-    for (let i = 0; i < n; ++i) {
-      sum += this.elements[i];
-    }
-    return sum / this.elements.length;
-  };
-
-  geomean(): number {
-    return Math.exp(this.log().sum() / this.elements.length);
   };
 
   sortElements(): number[] {
@@ -133,107 +60,23 @@ export class Vector {
     return count / sorted.length;
   };
 
-  ecdf(arg: Vector): Vector;
-  ecdf(arg: number): number;
-  ecdf(arg: Vector | number): Vector | number {
-    if (arg instanceof Vector) {
-      const result = new Vector([]);
-      for (let i = 0; i < arg.length(); i++) {
-        result.push(this._ecdf(arg.elements[i]));
-      }
-      return result;
-    } else {
-      return this._ecdf(arg);
+  ecdf(arg: Vector): Vector {
+    const result = new Vector([]);
+    for (let i = 0; i < arg.length(); i++) {
+      result.push(this._ecdf(arg.elements[i]));
     }
+    return result;
   };
 
   sort(): Vector {
     return new Vector(this.sortElements());
   };
 
-  min(): number {
-    return this.sortElements()[0];
-  };
-
   max(): number {
-    const maxElement = this.sortElements().pop();
-    if (maxElement === undefined) {
-      throw new Error("cannot find max of empty Vector");
-    }
-    return maxElement;
+    return this.sortElements().pop()!;
   };
 
   toString(): string {
     return "[" + this.elements.join(", ") + "]";
-  };
-
-  /*
-   * unbiased sample variance
-   */
-
-  variance(): number {
-    return this.ss() / (this.elements.length - 1);
-  };
-
-  /*
-   * biased sample variance
-   */
-
-  biasedVariance(): number {
-    return this.ss() / this.elements.length;
-  };
-
-  /*
-   * corrected sample standard deviation
-   */
-
-  sd(): number {
-    return Math.sqrt(this.variance());
-  };
-
-  /*
-   * uncorrected sample standard deviation
-   */
-
-  uncorrectedSd(): number {
-    return Math.sqrt(this.biasedVariance());
-  };
-
-  /*
-   * standard error of the mean
-   */
-
-  sem(): number {
-    return this.sd() / Math.sqrt(this.elements.length);
-  };
-
-  /*
-   * total sum of squares
-   */
-
-  ss(): number {
-    const m = this.mean();
-    let sum = 0;
-    const n = this.elements.length
-    for (let i = 0; i < n; ++i) {
-      sum += Math.pow(this.elements[i] - m, 2);
-    }
-    return sum;
-  };
-
-  /*
-   * residuals
-   */
-
-  res(): Vector {
-    return this.add(-this.mean());
-  };
-
-  kurtosis() {
-    return this.res().pow(4).mean() / Math.pow(this.res().pow(2).mean(), 2);
-  };
-
-  skewness() {
-    return this.res().pow(3).mean() / Math.pow(this.res().pow(2).mean(), 3 / 2);
   };
 }
